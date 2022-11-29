147f3bf811f371da3d27883d00d1fc0582f88ceb
Vladimir_Okunev <vova-okunev@mail.ru>
2022-11-29 19:14:29 +0900

    added task 64

---
+++ Task_64/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, 
+//которая выведет все чётные числа в промежутке от N до 1.
+
+void OddCount(int n)
+{
+	if(n==2) 
+    {Console.WriteLine(n);
+    return;
+    }
+    else
+    {
+        if(n%2==0) Console.WriteLine(n);
+    }
+	OddCount(n-1);
+}
+
+Console.Write("Enter a number: ");
+
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+OddCount(n);
